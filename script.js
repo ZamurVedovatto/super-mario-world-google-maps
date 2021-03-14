7c1f1a908926497f947098a1ba8f18df3b69fc01
Zamur Vedovatto <vedovatto@gmail.com>
2021-03-13 22:15:30 -0300

    add marker icons

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ function initMap() {
       lat: 34.66879900039354,
       lng: 135.43004324966117
     },
-    zoom: 8,
-    mapId: '408e75da6bf6e7b5'
+    zoom: 16,
+    mapId: '408e75da6bf6e7b5',
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: false
   })
+
+
+
+  const marker = new google.maps.Marker({
+    position: { lat: 34.66658415352165, lng: 135.42928150230333 },
+    map,
+    title: "New Life!",
+    icon: {
+      url: './assets/sm (1).svg',
+      scaledSize: new google.maps.Size(50, 40)
+    },
+    animation: google.maps.Animation.DROP
+  });
+  const infowindow = new google.maps.InfoWindow({
+    content: "New Life!"
+  });
+  marker.addListener("click", () => {
+    infowindow.open(map, marker)
+  });
 }
+
+
