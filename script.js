9495b479367c9427e697a8c3aec9d80540e3a48c
Zamur Vedovatto <vedovatto@gmail.com>
2021-03-13 22:28:08 -0300

    set different locations and markers

--- script.js
+++ script.js
@@ -11,24 +11,82 @@ function initMap() {
     streetViewControl: false
   })
 
+  // name
+  // latitude, longitude
+  // image url
+  // scaledSize innerWidth, height
+  const markers = [
+    [
+      "New Life!",
+      34.67002111165768, 135.43029001312496,
+      './assets/sm (1).svg',
+      50, 40
+    ],
+    [
+      "Toad\'s House",
+      34.67008287853562, 135.43255379767922,
+      './assets/sm (2).svg',
+      50, 40
+    ],
+    [
+      "Super Star\'s House",
+      34.667726880711314, 135.43253234000642,
+      './assets/sm (3).svg',
+      50, 40
+    ],
+    [
+      "Mario is here",
+      34.66894459581794, 135.42865923006943,
+      './assets/sm (4).svg',
+      50, 40
+    ],
+    [
+      "Mario\'s Garage",
+      34.665970868715966, 135.42954972348986,
+      './assets/sm (5).svg',
+      50, 40
+    ],
+    [
+      "Don\'t come here",
+      34.67041818364065, 135.42533329078836,
+      './assets/sm (6).svg',
+      50, 40
+    ],
+    [
+      "Mario\'s Bank",
+      34.66541493756377, 135.4253440196248,
+      './assets/sm (7).svg',
+      50, 40
+    ],
+  ];
+
+  for (let i = 0; i < markers.length; i++) {
+    const currMarker = markers[i];
+
+    const marker = new google.maps.Marker({
+      position: { lat: currMarker[1], lng: currMarker[2] },
+      map,
+      title: currMarker[0],
+      icon: {
+        url: currMarker[3],
+        scaledSize: new google.maps.Size(currMarker[4], currMarker[5])
+      },
+      animation: google.maps.Animation.DROP
+    });
+    const infowindow = new google.maps.InfoWindow({
+      content: currMarker[0]
+    });
+    marker.addListener("click", () => {
+      infowindow.open(map, marker)
+    });
 
 
-  const marker = new google.maps.Marker({
-    position: { lat: 34.66658415352165, lng: 135.42928150230333 },
-    map,
-    title: "New Life!",
-    icon: {
-      url: './assets/sm (1).svg',
-      scaledSize: new google.maps.Size(50, 40)
-    },
-    animation: google.maps.Animation.DROP
-  });
-  const infowindow = new google.maps.InfoWindow({
-    content: "New Life!"
-  });
-  marker.addListener("click", () => {
-    infowindow.open(map, marker)
-  });
+    console.log(marker)
+  }
+
+
+
+
 }
 
 
