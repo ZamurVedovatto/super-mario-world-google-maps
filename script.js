04b3e2d08c4d79745856d27700fbac7d9583d500
Zamur Vedovatto <vedovatto@gmail.com>
2021-03-13 21:56:40 -0300

    set lang and long

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {
+      lat: 34.66879900039354,
+      lng: 135.43004324966117
+    },
     zoom: 8,
     mapId: '408e75da6bf6e7b5'
   })
